1a529d1e82ec17982925f866560cfc7557708a0d
Honzo <Honzo@Kelterm>
2014-02-17 02:16:31 +0100

    Agregado asteroides 5

---
+++ Asteroides5/src/com/ejemplo/asteroides5/AcercaDe.java
@@ -0,0 +1,23 @@
+package com.ejemplo.asteroides5;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.estudioskelon.t5_asteroides.R;
+
+public class AcercaDe extends Activity {
+
+	/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	
+		//View vista = findViewById(R.layout.acercade);
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.acercade);
+		//vista.setBackgroundResource(R.drawable.degradado);
+
+
+	}
+}
---
+++ Asteroides5/src/com/ejemplo/asteroides5/Juego.java
@@ -0,0 +1,14 @@
+package com.ejemplo.asteroides5;
+
+import com.estudioskelon.t5_asteroides.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Juego extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.juego);
+	}
+}
